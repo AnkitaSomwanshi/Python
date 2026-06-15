342134556c5217c93e6271a17598dea040fe5e3c
AnkitaSomwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-06-15 15:58:24 +0530

    Add files via upload

---
+++ Question 6.py
@@ -0,0 +1,14 @@
+# Write a program that accepts two numbers from user and prints their addition, subtraction, multiplication and division.
+
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 // num2
+
+print("Addition: ", addition)
+print("subtraction: ", subtraction)
+print("multiplication: ", multiplication)
+print("division: ", division)
