5699bb7c9e31a8e9e749d5a44db326833ce8de67
AnkitaSomwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-06-15 15:57:36 +0530

    Add files via upload

---
+++ Question 1.py
@@ -0,0 +1,9 @@
+# Write a program to display value, type and Memory address for a variable using appropriate built-in functions.
+
+name="Sumit"
+
+print(name)
+
+print(type(name))
+
+print(id(name))
