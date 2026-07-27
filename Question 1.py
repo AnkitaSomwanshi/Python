7d10ee65e61898757982bffd261d6c4dd159cc05
Ankita Somwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-07-27 17:42:17 +0530

    Delete Question 1.py

--- Question 1.py
+++
@@ -1,9 +0,0 @@
-# Write a program to display value, type and Memory address for a variable using appropriate built-in functions.
-
-name="Sumit"
-
-print(name)
-
-print(type(name))
-
-print(id(name))
