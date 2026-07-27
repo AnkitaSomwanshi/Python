c15014948261d927ca6edb1b9065b54b69298ae3
Ankita Somwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-07-27 17:43:32 +0530

    Delete Question 5.py

--- Question 5.py
+++
@@ -1,7 +0,0 @@
-# write a function that does not return anything but print a message
-
-def main():
-    print("Sumit is very good Man, He always keeps me happy.")
-
-if __name__ == "__main__":
-    main()
