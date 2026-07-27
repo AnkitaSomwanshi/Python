3e8b551e1fec440b1fd966d1b447aec59dec1c74
Ankita Somwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-07-27 17:54:52 +0530

    Question 27.py

--- Question 27.py
+++
@@ -1,9 +0,0 @@
-# Write a lambda function which accepts one number and returns True if number is even otherwise False
-
-Even = lambda x : True if x % 2 == 0 else False
-
-num = int(input("Enter a number : "))
-
-Res = Even(num)
-
-print(Res)
