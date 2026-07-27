a8f35a569f7e6582ee4508bff324db3cea46790e
Ankita Somwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-07-27 17:55:08 +0530

    Question 28.py

--- Question 28.py
+++
@@ -1,9 +0,0 @@
-# Write a program which accepts one number and returns True if number is odd otherwise False
-
-Odd = lambda z : True if z % 2 != 0 else False
-
-num = int(input("Enter a number : "))
-
-Res = Odd(num)
-
-print(Res)
