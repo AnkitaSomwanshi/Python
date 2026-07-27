fd7a451e69fb1c4381358b41db7206eeb75b04a5
Ankita Somwanshi <99323355+AnkitaSomwanshi@users.noreply.github.com>
2026-07-27 17:43:45 +0530

    Delete Question 6.py

--- Question 6.py
+++
@@ -1,9 +0,0 @@
-# write a program to display data type, memory address and size in bytes enter by the user
-import sys
-
-name = input("Enter your name : ")
-
-print(name)
-print(type(name))
-print(id(name))
-print(sys.getsizeof(name))
